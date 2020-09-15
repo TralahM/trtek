5a0fdf683d1ac3743db3581b43a6ff44c818cbf7
TralahM <musyoki.brian@tralahtek.com>
2020-09-15 10:35:11 +0300

    experimenting with macros
    Signed-off-by: TralahM <musyoki.brian@tralahtek.com>

--- src/macros.lisp
+++ src/macros.lisp
@@ -1,11 +1,24 @@
 (in-package :trtek)
 
-
 (defmacro nil! (var)
-  "Sets its argument to nil"
-  (list 'setq var nil))
-
-;; with backquotes the macro can be defined as
-
-(defmacro nill! (var)
+  "set var to nil"
   `(setq ,var nil))
+
+(defmacro while (test &body body)
+  `(do ()
+       ((not ,test))
+     ,@body))
+
+
+(defmacro nif (expr pos zero neg)
+  "Numerical If macro"
+  `(case (truncate (signum ,expr))
+     (1 ,pos)
+     (0 ,zero)
+     (-1 ,neg)))
+
+(defmacro avg (&rest args)
+  "Calculate the average of list macro."
+  `(/ (+ ,args) ,(length args)))
+
+; (macroexpand 'avg )
